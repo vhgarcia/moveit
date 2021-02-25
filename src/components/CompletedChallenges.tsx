761f05d2a9bcef0dbca6e26b544124d933ec8657
Victor Garcia <vhgarcia2005@gmail.com>
2021-02-24 22:30:37 -0300

    eslint + prettier

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,10 @@
-import styles from '../styles/components/CompletedChallenges.module.css'
+import styles from '../styles/components/CompletedChallenges.module.css';
 
-export function CompletedChallenges () {
+export function CompletedChallenges() {
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios Completos</span>
       <span>5</span>
     </div>
-  )
+  );
 }
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react'
-import styles from '../styles/components/Countdown.module.css'
+import { useEffect, useState } from 'react';
+import styles from '../styles/components/Countdown.module.css';
 
-export function Countdown () {
+export function Countdown() {
   const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  const minutes = Math.floor( time / 60);
+  const minutes = Math.floor(time / 60);
   const seconds = time % 60;
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
-  function startCountdown () {
-    setActive(true);
+  function startCountdown() {
+    setIsActive(true);
   }
 
   useEffect(() => {
-    if( active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
-        setTime(time-1);
-      }, 1000)
+        setTime(time - 1);
+      }, 1000);
     }
-  }, [active, time])
+  }, [isActive, time]);
 
   return (
     <div>
@@ -37,11 +37,13 @@ export function Countdown () {
         </div>
       </div>
 
-      <button type="button" 
-      className={styles.countdownButton}
-      onClick={startCountdown}
-      >INICIAR
+      <button
+        type="button"
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
+        INICIAR
       </button>
     </div>
-  )
+  );
 }
